99d59abfc112014a033e7e22fa7c1123c8456b5c
Sheyla Arellano <sheylarellanopsico@gmail.com>
2024-06-23 17:04:32 +0200

    Lab Tests Jasmine

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,12 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+
+      it("should return undefined if any of the two arguments is not a number",() => {
+        expect(add("a", 9)).toEqual(undefined);
+        expect(add(5, "b")).toEqual(undefined);
+        expect(add(false, "b")).toEqual(undefined);
+      })
     });
   });
   
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,30 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 2", () => {
     describe("Function - divide", () => {
         
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        //Each `it` block represents 1 test. You can use the following as a template:
+        it("should be defined", () => {
+            expect(divide).toBeDefined();
+        });
+
+        it("should take two numbers as arguments", () => {
+            expect(divide.length).toBe(2);
+        });
+
+        it("should return the division of the two numbers", () => {
+            expect(divide(4, 2)).toEqual(2);
+            expect(divide(10, 5)).toEqual(2);
+            expect(divide(22, 11)).toEqual(2);
+        });
+
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(divide(4)).toEqual(undefined);
+            expect(divide(undefined, 5)).toEqual(undefined);
+            expect(divide(22, undefined)).toEqual(undefined);
+        });
 
     })    
 })
-
-*/
